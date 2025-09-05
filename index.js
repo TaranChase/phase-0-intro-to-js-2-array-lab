7fd78cf472c1476df892b822de68f2422205e9fa
TaranChase <chase.taran@gmail.com>
2025-09-05 23:06:26 +0300

    Creating arrays and practicing different outputs using the array methods.

--- index.js
+++ index.js
@@ -1,1 +1,69 @@
-// Write your solution here!
+const cats =["Milo", "Otis", "Garfield"];
+
+// append cat to end of array
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+constant.log(destructivelyAppendCat("Tom"));
+
+
+// adds elements to the beginning of the array
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+constant.log(destructivelyPrependCat("Snowy"));
+
+
+// removes last element in array
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+console.log(destructivelyRemoveLastCat());
+
+
+// removes first element from array
+function destructivelyRemoveFirstCat(name) {
+    cats.shift();
+}
+
+console.log(destructivelyRemoveFirstCat());
+
+//appending
+function appendCat(name) {
+    return[...cats, name];
+
+}
+
+console.log(appendCat("Mr.Chief"));
+
+console.log(cats);
+
+
+// prepending
+function prependCat(name) {
+    return [name, ...cats];
+    
+}
+
+console.log(prependCat("whiskers"));
+
+
+// removing last cat
+
+function removeLastCat() {
+    return cats.slice(0, cats.length -1);
+}
+
+console.log(removeLastCat());
+
+
+// removing first cat 
+
+function removeFirstCat() {
+    return cats.slice(1);
+}
+
+console.log(removeFirstCat());
